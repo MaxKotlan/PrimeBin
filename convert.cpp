78b29e70e17dd10c84ac952b17e5292ce25c9747
Max Kotlan <max.maxkot@gmail.com>
2020-07-11 11:56:52 -0400

    Command Line Parameters

--- convert.cpp
+++ convert.cpp
@@ -7,6 +7,7 @@
 
 
 Converter::Converter(std::string filename) : inputfilename(filename){
+    std::cout << "Reading " << filename << " Into Memory... " << std::flush; 
     input = fopen(filename.c_str(), "rb+");
     fseek(input, 0L, SEEK_END);
     uint64_t size = ftell(input);
@@ -14,23 +15,30 @@ Converter::Converter(std::string filename) : inputfilename(filename){
     buffer.resize(size);
     fread(buffer.data(), buffer.size(), 1, input);
     fclose(input);
+    std::cout << "Done!" << std::endl;
     asciiToBinary();
 };
 
 void Converter::swapEndianess() {
-    std::cout << "Swapping Endieness..." << std::endl; 
+    std::cout << "Swapping Endieness... " << std::flush; 
     for (uint32_t &primenumber : outputdata)
         primenumber = htonl(primenumber);
+    std::cout << "Done!" << std::endl;
 }
 
 void Converter::write(){
-    output = fopen((inputfilename.substr(0, inputfilename.find('.')) + ".bin").c_str(), "wb+");
+    std::string outputfilename = inputfilename.substr(0, inputfilename.find('.')) + ".bin";
+    std::cout << "Writing to " << outputfilename << "... " << std::flush; 
+    output = fopen(outputfilename.c_str(), "wb+");
     fwrite(outputdata.data(), outputdata.size(), sizeof(uint32_t), output);
     fclose(output);
+    std::cout << "Done!" << std::endl;
 }
 
 void Converter::asciiToBinary(){
+    std::cout << "Converting... " << std::flush; 
     uint32_t result = 0;
+    bool previous = false;
     for (uint64_t i = 0; i < buffer.size(); i++){
         if(buffer[i] >= '0' && buffer[i] <= '9'){
             result *= 10;
@@ -40,5 +48,5 @@ void Converter::asciiToBinary(){
             result = 0;
         }
     }
-    
+    std::cout << "Done!" << std::endl;    
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
+#include <string>
 #include "convert.h"
 
+struct Startup{
+    bool swapEndianess = false;
+} startup;
+
+void help(std::string progname){
+    std::cout << "Usage: " << progname << " [filename.txt]" << std::endl;
+    exit(-1);
+}
+
+std::string parseArgs(int argc, char** argv){
+    if (argc < 2) help(argv[0]);
+    std::string filename;
+    for (int i = 0; i < argc; i++){
+        if (std::string(argv[i]).find("--") == std::string::npos)
+            filename = argv[i];
+        else if (std::string(argv[i]) == "--swapendianess")
+            startup.swapEndianess = true;
+    }
+    if (filename == "") exit(-1);
+    return filename;
+}
+
 int main(int argc, char** argv){
-    Converter conv("primes.txt");
-    conv.swapEndianess();
+    std::string filename = parseArgs(argc, argv);
+    Converter conv(filename);
+    if (startup.swapEndianess)
+        conv.swapEndianess();
     conv.write();
 
+/*
     //test reading the file
     FILE* test = fopen("primes.bin", "rb+");
     fseek(test, 0L, SEEK_END);
     uint64_t size = ftell(test);
     fseek(test, 0L, SEEK_SET);
-    std::vector<uint32_t> rbuf(size);
-    fread(rbuf.data(), rbuf.size(), sizeof(uint32_t), test);
+    std::vector<uint32_t> rbuf(16);
+    fread(rbuf.data(), 8, sizeof(uint32_t), test);
     fclose(test);
     for (uint64_t i = 0; i < rbuf.size(); i++){
         std::cout << rbuf[i] << std::endl;
         if (i > 10) break;
-    }
+    }*/
 
 
 }
