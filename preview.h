ea57282a7a6ad0b946c8ecba451ad93a4a49125e
Max Kotlan <max.maxkot@gmail.com>
2020-07-14 11:38:14 -0400

    good enough...

--- preview.h
+++ preview.h
@@ -12,6 +12,7 @@ class Previewer{
         void PrintTop();
 
     private:
+        uint32_t rows = 1;
         uint32_t bytepeeksize = 32;
         std::vector<T>* _outputdata;
 };
@@ -23,7 +24,7 @@ template <class T>
 void Previewer<T>::PrintTop(){
     std::cout << std::endl <<"Map Preview: " << std::endl;
     for (uint32_t i = 0; i < _outputdata->size() && i < bytepeeksize; i++){
-        std::cout << "\t" << std::hex << std::hexfloat << std::setfill('0') << std::setw(2) << i*sizeof(T) << " " << std::setw(sizeof(T)*2) << _outputdata->operator[](i) << " (" << std::dec << std::defaultfloat << _outputdata->operator[](i) << ")" <<  std::endl;
+        std::cout << "\t" << std::hex << std::hexfloat << std::setfill('0') << std::setw(2) << i*sizeof(T) << " " << std::setw(sizeof(T)*2) << _outputdata->operator[](i) << " (" << std::dec << std::defaultfloat << _outputdata->operator[](i) << ")" << std::endl;
     }
     
 };
